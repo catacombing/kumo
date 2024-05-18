catacombing/kumo

commit 50bfcd54c486d808db59bf103799cc07a69cf29d

Draw UI behind engine buffer
This patch changes the size of the UI surface to stretch all the way
behind the engine's surface. This makes sure that no parts of the window
are transparent even if the engine surface has a (temporarily) incorrect
size.

Closes #16.

src/window.rs
@@ -10,7 +10,6 @@ use funq::StQueueHandle;
 use glutin::display::Display;
 use indexmap::IndexMap;
 use smithay_client_toolkit::compositor::{CompositorState, Region};
-use smithay_client_toolkit::reexports::client::protocol::wl_buffer::WlBuffer;
 use smithay_client_toolkit::reexports::client::protocol::wl_surface::WlSurface;
 use smithay_client_toolkit::reexports::client::{Connection, QueueHandle};
 use smithay_client_toolkit::reexports::protocols::wp::text_input::zv3::client as _text_input;
@@ -24,7 +23,7 @@ use crate::engine::webkit::{WebKitEngine, WebKitError};
 use crate::engine::{Engine, EngineId};
 use crate::tlds::TLDS;
 use crate::ui::tabs::TabsUi;
-use crate::ui::{Ui, UI_HEIGHT};
+use crate::ui::{Ui, TOOLBAR_HEIGHT};
 use crate::wayland::protocols::ProtocolStates;
 use crate::{Position, Size, State};
 
@@ -34,9 +33,6 @@ const SEARCH_URI: &str = "https://duckduckgo.com/?q=";
 // Default window size.
 const DEFAULT_WIDTH: u32 = 1280;
 const DEFAULT_HEIGHT: u32 = 720;
-
-/// Fallback color when waiting for engine frame.
-const BACKGROUND_COLOR: [u32; 4] = [402653184, 402653184, 402653184, u32::MAX];
 
 #[funq::callbacks(State)]
 pub trait WindowHandler {
@@ -79,8 +75,6 @@ pub struct Window {
     scale: f64,
     size: Size,
 
-    fallback_buffer: Option<WlBuffer>,
-
     queue: StQueueHandle<State>,
 
     tabs_ui: TabsUi,
@@ -112,10 +106,11 @@ impl Window {
 
         // Create UI renderer.
         let id = WindowId::new();
-        let ui_surface =
+        let (ui_subsurface, ui_surface) =
             protocol_states.subcompositor.create_subsurface(surface.clone(), &wayland_queue);
-        let ui_viewport = protocol_states.viewporter.viewport(&wayland_queue, &ui_surface.1);
+        let ui_viewport = protocol_states.viewporter.viewport(&wayland_queue, &ui_surface);
         let mut ui = Ui::new(id, queue.handle(), egl_display.clone(), ui_surface, ui_viewport);
+        ui_subsurface.place_below(&surface);
 
         // Create tabs UI renderer.
         let (_, tabs_ui_surface) =
@@ -132,23 +127,14 @@ impl Window {
         xdg.set_app_id("Kumo");
         xdg.commit();
 
-        // Create single-pixel buffer as fallback when waiting for engine rendering.
-        let fallback_buffer = protocol_states.single_pixel_buffer.as_ref().map(|spb| {
-            let [r, g, b, a] = BACKGROUND_COLOR;
-            spb.create_u32_rgba_buffer(r, g, b, a, &wayland_queue, ())
-        });
-
         let size = Size::new(DEFAULT_WIDTH, DEFAULT_HEIGHT);
         let active_tab = EngineId::new(id);
 
         // Resize UI elements to the initial window size.
-        tabs_ui.set_size(&protocol_states.compositor, Size::new(size.width, size.height));
-        let ui_pos = Position::new(0, (size.height - UI_HEIGHT) as i32);
-        let ui_size = Size::new(size.width, UI_HEIGHT);
-        ui.set_geometry(&protocol_states.compositor, ui_pos, ui_size);
+        tabs_ui.set_size(&protocol_states.compositor, size);
+        ui.set_size(&protocol_states.compositor, size);
 
         let mut window = Self {
-            fallback_buffer,
             wayland_queue,
             egl_display,
             connection,
@@ -197,7 +183,7 @@ impl Window {
     /// Add a tab to the window.
     pub fn add_tab(&mut self, focus_uribar: bool) -> Result<EngineId, WebKitError> {
         // Create a new browser engine.
-        let size = Size::new(self.size.width, self.size.height - UI_HEIGHT);
+        let size = self.engine_size();
         let engine_id = EngineId::new(self.id);
         let engine =
             WebKitEngine::new(&self.egl_display, self.queue.clone(), engine_id, size, self.scale)?;
@@ -287,30 +273,20 @@ impl Window {
         let surface = self.xdg.wl_surface();
         let tabs_ui_visible = self.tabs_ui.visible();
         if !tabs_ui_visible {
+            let engine_size: Size<i32> = self.engine_size().into();
             let engine = self.tabs.get_mut(&self.active_tab).unwrap();
-            match (engine.wl_buffer(), &self.fallback_buffer) {
-                // Render buffer if one is attached and requires redraw.
-                (Some(engine_buffer), _) if engine.dirty() || self.dirty => {
-                    // Attach engine buffer to primary surface.
-                    surface.attach(Some(engine_buffer), 0, 0);
-                    let height = (self.size.height - UI_HEIGHT) as i32;
-                    surface.damage(0, 0, self.size.width as i32, height);
 
-                    // Request new engine frame.
-                    engine.frame_done();
+            // Render buffer if one is attached and requires redraw.
+            let dirty = engine.dirty() || self.dirty;
+            if let Some(engine_buffer) = engine.wl_buffer().filter(|_| dirty) {
+                // Attach engine buffer to primary surface.
+                surface.attach(Some(engine_buffer), 0, 0);
+                surface.damage(0, 0, engine_size.width, engine_size.height);
 
-                    self.stalled = false;
-                },
-                // Attach fallback buffer while engine is loading.
-                //
-                // We don't modify `stalled` here since this content never changes.
-                (None, Some(buffer)) => {
-                    surface.attach(Some(buffer), 0, 0);
-                    let height = (self.size.height - UI_HEIGHT) as i32;
-                    surface.damage(0, 0, self.size.width as i32, height);
-                },
-                // Ignore if buffer is attached but not dirty.
-                _ => (),
+                // Request new engine frame.
+                engine.frame_done();
+
+                self.stalled = false;
             }
         }
 
@@ -381,17 +357,15 @@ impl Window {
         self.size = size;
 
         // Resize window's browser engines.
-        let engine_size = Size::new(self.size.width, self.size.height - UI_HEIGHT);
+        let engine_size = self.engine_size();
         for engine in self.tabs.values_mut() {
             engine.set_size(engine_size);
         }
         self.update_primary_surface(compositor);
 
         // Resize UI element surface.
-        self.tabs_ui.set_size(compositor, Size::new(self.size.width, self.size.height));
-        let ui_pos = Position::new(0, (self.size.height - UI_HEIGHT) as i32);
-        let ui_size = Size::new(self.size.width, UI_HEIGHT);
-        self.ui.set_geometry(compositor, ui_pos, ui_size);
+        self.tabs_ui.set_size(compositor, self.size);
+        self.ui.set_size(compositor, self.size);
 
         self.unstall();
     }
@@ -716,7 +690,7 @@ impl Window {
 
     /// Update primary surface attributes.
     fn update_primary_surface(&self, compositor: &CompositorState) {
-        let engine_size = Size::new(self.size.width, self.size.height - UI_HEIGHT);
+        let engine_size = self.engine_size();
 
         // Update browser's viewporter logical render size.
         self.viewport.set_destination(engine_size.width as i32, engine_size.height as i32);
@@ -726,6 +700,11 @@ impl Window {
             region.add(0, 0, engine_size.width as i32, engine_size.height as i32);
             self.xdg.wl_surface().set_opaque_region(Some(region.wl_region()));
         }
+    }
+
+    /// Size allocated to the browser engine's buffer.
+    pub fn engine_size(&self) -> Size {
+        Size::new(self.size.width, self.size.height - TOOLBAR_HEIGHT as u32)
     }
 }
 

src/ui/mod.rs
@@ -10,7 +10,6 @@ use glutin::display::Display;
 use pangocairo::cairo::{Context, Format, ImageSurface};
 use pangocairo::pango::{Alignment, Layout, SCALE as PANGO_SCALE};
 use smithay_client_toolkit::compositor::{CompositorState, Region};
-use smithay_client_toolkit::reexports::client::protocol::wl_subsurface::WlSubsurface;
 use smithay_client_toolkit::reexports::client::protocol::wl_surface::WlSurface;
 use smithay_client_toolkit::reexports::protocols::wp::text_input::zv3::client as _text_input;
 use smithay_client_toolkit::reexports::protocols::wp::viewporter::client::wp_viewport::WpViewport;
@@ -23,8 +22,8 @@ use crate::{gl, rect_contains, Position, Size, State, WindowId};
 mod renderer;
 pub mod tabs;
 
-/// Logical height of the UI surface.
-pub const UI_HEIGHT: u32 = 50;
+/// Logical height of the non-browser UI.
+pub const TOOLBAR_HEIGHT: f64 = 50.;
 
 /// Logical height of the UI/content separator.
 const SEPARATOR_HEIGHT: f64 = 1.5;
@@ -95,7 +94,6 @@ impl UiHandler for State {
 pub struct Ui {
     renderer: Renderer,
 
-    subsurface: WlSubsurface,
     surface: WlSurface,
     viewport: WpViewport,
 
@@ -122,14 +120,13 @@ impl Ui {
         window_id: WindowId,
         queue: MtQueueHandle<State>,
         display: Display,
-        (subsurface, surface): (WlSubsurface, WlSurface),
+        surface: WlSurface,
         viewport: WpViewport,
     ) -> Self {
         let uribar = Uribar::new(window_id, queue.clone());
         let renderer = Renderer::new(display, surface.clone());
 
         let mut ui = Self {
-            subsurface,
             window_id,
             viewport,
             renderer,
@@ -153,13 +150,10 @@ impl Ui {
         ui
     }
 
-    /// Update the surface geometry.
-    pub fn set_geometry(&mut self, compositor: &CompositorState, position: Position, size: Size) {
+    /// Update the logical UI size.
+    pub fn set_size(&mut self, compositor: &CompositorState, size: Size) {
         self.size = size;
         self.dirty = true;
-
-        // Update subsurface location.
-        self.subsurface.set_position(position.x, position.y);
 
         // Update opaque region.
         if let Ok(region) = Region::new(compositor) {
@@ -207,8 +201,9 @@ impl Ui {
         // Calculate target positions/sizes before partial mutable borrows.
         let prev_button_pos = self.prev_button_position();
         let tabs_button_pos = self.tabs_button_position();
-        let separator_size = self.separator_size();
+        let separator_pos = self.separator_position();
         let uribar_pos = self.uribar_position();
+        let separator_size = self.separator_size();
 
         // Render the UI.
         let physical_size = self.size * self.scale;
@@ -229,9 +224,9 @@ impl Ui {
                 gl::Clear(gl::COLOR_BUFFER_BIT);
 
                 // Draw UI elements.
+                renderer.draw_texture_at(separator_texture, separator_pos.into(), separator_size);
                 renderer.draw_texture_at(prev_button_texture, prev_button_pos.into(), None);
                 renderer.draw_texture_at(tabs_button_texture, tabs_button_pos.into(), None);
-                renderer.draw_texture_at(separator_texture, (0., 0.).into(), separator_size);
                 renderer.draw_texture_at(uribar_texture, uribar_pos.into(), None);
             }
         });
@@ -413,9 +408,9 @@ impl Ui {
 
     /// Physical position of the URI bar.
     fn uribar_position(&self) -> Position {
-        let available_height = (self.size.height as f64 - SEPARATOR_HEIGHT) * self.scale;
+        let available_height = (TOOLBAR_HEIGHT - SEPARATOR_HEIGHT) * self.scale;
         let vertical_padding = available_height * (1. - URIBAR_HEIGHT_PERCENTAGE) / 2.;
-        let y = SEPARATOR_HEIGHT * self.scale + vertical_padding;
+        let y = self.size.height as f64 * self.scale - available_height + vertical_padding;
 
         let horizontal_padding = X_PADDING * self.scale;
         let prev_button_x = self.prev_button_position().x;
@@ -427,7 +422,7 @@ impl Ui {
 
     /// Physical size of the URI bar.
     fn uribar_size(&self) -> Size {
-        let available_height = (self.size.height as f64 - SEPARATOR_HEIGHT) * self.scale;
+        let available_height = (TOOLBAR_HEIGHT - SEPARATOR_HEIGHT) * self.scale;
         let height = available_height * URIBAR_HEIGHT_PERCENTAGE;
 
         let tabs_button_start = self.tabs_button_position().x as f64;
@@ -440,27 +435,33 @@ impl Ui {
 
     /// Physical position of the tabs button.
     fn tabs_button_position(&self) -> Position {
-        let available_height = (self.size.height as f64 - SEPARATOR_HEIGHT) * self.scale;
+        let available_height = (TOOLBAR_HEIGHT - SEPARATOR_HEIGHT) * self.scale;
         let vertical_padding = (available_height - TABS_BUTTON_SIZE as f64 * self.scale) / 2.;
 
         let x = ((self.size.width - TABS_BUTTON_SIZE) as f64 - X_PADDING) * self.scale;
-        let y = SEPARATOR_HEIGHT * self.scale + vertical_padding;
+        let y = self.size.height as f64 * self.scale - available_height + vertical_padding;
 
         Position::new(x.round() as i32, y.round() as i32)
     }
 
     /// Physical position of the previous page button.
     fn prev_button_position(&self) -> Position {
-        let available_height = (self.size.height as f64 - SEPARATOR_HEIGHT) * self.scale;
+        let available_height = (TOOLBAR_HEIGHT - SEPARATOR_HEIGHT) * self.scale;
         let vertical_padding = (available_height - PREV_BUTTON_SIZE as f64 * self.scale) / 2.;
 
         let x = (X_PADDING * self.scale).round() as i32;
-        let y = SEPARATOR_HEIGHT * self.scale + vertical_padding;
+        let y = self.size.height as f64 * self.scale - available_height + vertical_padding;
 
         Position::new(x, y.round() as i32)
     }
 
-    /// Physical size of the UI/content separator.
+    /// Physical position of the toolbar separator.
+    fn separator_position(&self) -> Position {
+        let y = (self.size.height as f64 - TOOLBAR_HEIGHT) * self.scale;
+        Position::new(0, y.round() as i32)
+    }
+
+    /// Physical size of the toolbar separator.
     fn separator_size(&self) -> Size<f32> {
         let mut physical_size = self.size * self.scale;
         physical_size.height = (SEPARATOR_HEIGHT * self.scale).round() as u32;

src/wayland/protocols/mod.rs
@@ -2,13 +2,11 @@
 
 use std::sync::{Arc, Mutex};
 
-use _spb::wp_single_pixel_buffer_manager_v1::{self, WpSinglePixelBufferManagerV1};
 use _text_input::zwp_text_input_manager_v3::{self, ZwpTextInputManagerV3};
 use _text_input::zwp_text_input_v3::{self, ZwpTextInputV3};
 use smithay_client_toolkit::compositor::{CompositorHandler, CompositorState};
 use smithay_client_toolkit::output::{OutputHandler, OutputState};
 use smithay_client_toolkit::reexports::client::globals::GlobalList;
-use smithay_client_toolkit::reexports::client::protocol::wl_buffer::{self, WlBuffer};
 use smithay_client_toolkit::reexports::client::protocol::wl_keyboard::WlKeyboard;
 use smithay_client_toolkit::reexports::client::protocol::wl_output::{Transform, WlOutput};
 use smithay_client_toolkit::reexports::client::protocol::wl_pointer::WlPointer;
@@ -16,7 +14,6 @@ use smithay_client_toolkit::reexports::client::protocol::wl_seat::WlSeat;
 use smithay_client_toolkit::reexports::client::protocol::wl_surface::WlSurface;
 use smithay_client_toolkit::reexports::client::protocol::wl_touch::WlTouch;
 use smithay_client_toolkit::reexports::client::{Connection, Dispatch, QueueHandle};
-use smithay_client_toolkit::reexports::protocols::wp::single_pixel_buffer::v1::client as _spb;
 use smithay_client_toolkit::reexports::protocols::wp::text_input::zv3::client as _text_input;
 use smithay_client_toolkit::registry::{ProvidesRegistryState, RegistryState};
 use smithay_client_toolkit::seat::keyboard::{
@@ -44,7 +41,6 @@ pub mod viewporter;
 
 #[derive(Debug)]
 pub struct ProtocolStates {
-    pub single_pixel_buffer: Option<WpSinglePixelBufferManagerV1>,
     pub fractional_scale: FractionalScaleManager,
     pub subcompositor: SubcompositorState,
     pub compositor: CompositorState,
@@ -60,7 +56,6 @@ pub struct ProtocolStates {
 impl ProtocolStates {
     pub fn new(globals: &GlobalList, queue: &QueueHandle<State>) -> Self {
         // SPB is optional for rendering a fallback when the engine is too slow.
-        let single_pixel_buffer = globals.bind(queue, 1..=1, ()).ok();
         let text_input = TextInputManager::new(globals, queue);
         let registry = RegistryState::new(globals);
         let compositor = CompositorState::bind(globals, queue).unwrap();
@@ -73,7 +68,6 @@ impl ProtocolStates {
         let seat = SeatState::new(globals, queue);
 
         Self {
-            single_pixel_buffer,
             fractional_scale,
             subcompositor,
             compositor,
@@ -709,37 +703,6 @@ impl Dispatch<ZwpTextInputV3, Arc<Mutex<TextInputState>>> for State {
                 let (text, cursor_begin, cursor_end) = preedit_string;
                 state.preedit_string(surface, text, cursor_begin, cursor_end);
             },
-            _ => unreachable!(),
-        }
-    }
-}
-
-impl Dispatch<WpSinglePixelBufferManagerV1, ()> for State {
-    fn event(
-        _state: &mut State,
-        _manager: &WpSinglePixelBufferManagerV1,
-        _event: wp_single_pixel_buffer_manager_v1::Event,
-        _data: &(),
-        _connection: &Connection,
-        _queue: &QueueHandle<State>,
-    ) {
-        // No events.
-    }
-}
-
-impl Dispatch<WlBuffer, ()> for State {
-    fn event(
-        _state: &mut State,
-        _buffer: &WlBuffer,
-        event: wl_buffer::Event,
-        _data: &(),
-        _connection: &Connection,
-        _queue: &QueueHandle<State>,
-    ) {
-        match event {
-            // We don't need to handle release since we only use non-mutable SHM buffers for single
-            // pixel buffers.
-            wl_buffer::Event::Release => (),
             _ => unreachable!(),
         }
     }

commit c62468e7de87f482bc0df64e3a73282b438fe1c9

Switch browser UI to primary surface
This fixes a regression introduced in 50bfcd5 where Kumo would not
render the first frame in Sway and thus stall indefinitely. This was
caused by the UI delivering the first frame, which wasn't counted by
Sway since it's a subsurface and the parent surface was not mapped yet.

Since the UI surface has changed to be a window-sized surface that is
always able to render in 50bfcd5 anyway, it should not be an issue to
just make it the primary surface instead.

src/window.rs
@@ -68,10 +68,11 @@ pub struct Window {
     text_input: Option<TextInput>,
     wayland_queue: QueueHandle<State>,
     initial_configure_done: bool,
+    engine_viewport: WpViewport,
+    engine_surface: WlSurface,
     connection: Connection,
     egl_display: Display,
     xdg: XdgWindow,
-    viewport: WpViewport,
     scale: f64,
     size: Size,
 
@@ -96,21 +97,14 @@ impl Window {
         queue: StQueueHandle<State>,
         wayland_queue: QueueHandle<State>,
     ) -> Result<Self, WebKitError> {
+        // Create UI renderer.
+        let id = WindowId::new();
         let surface = protocol_states.compositor.create_surface(&wayland_queue);
+        let ui_viewport = protocol_states.viewporter.viewport(&wayland_queue, &surface);
+        let mut ui = Ui::new(id, queue.handle(), egl_display.clone(), surface.clone(), ui_viewport);
 
         // Enable fractional scaling.
         protocol_states.fractional_scale.fractional_scaling(&wayland_queue, &surface);
-
-        // Enable viewporter for the browser surface.
-        let viewport = protocol_states.viewporter.viewport(&wayland_queue, &surface);
-
-        // Create UI renderer.
-        let id = WindowId::new();
-        let (ui_subsurface, ui_surface) =
-            protocol_states.subcompositor.create_subsurface(surface.clone(), &wayland_queue);
-        let ui_viewport = protocol_states.viewporter.viewport(&wayland_queue, &ui_surface);
-        let mut ui = Ui::new(id, queue.handle(), egl_display.clone(), ui_surface, ui_viewport);
-        ui_subsurface.place_below(&surface);
 
         // Create tabs UI renderer.
         let (_, tabs_ui_surface) =
@@ -119,6 +113,11 @@ impl Window {
             protocol_states.viewporter.viewport(&wayland_queue, &tabs_ui_surface);
         let mut tabs_ui =
             TabsUi::new(id, queue.handle(), egl_display.clone(), tabs_ui_surface, tabs_ui_viewport);
+
+        // Create engine surface.
+        let (_, engine_surface) =
+            protocol_states.subcompositor.create_subsurface(surface.clone(), &wayland_queue);
+        let engine_viewport = protocol_states.viewporter.viewport(&wayland_queue, &engine_surface);
 
         // Create XDG window.
         let decorations = WindowDecorations::RequestServer;
@@ -135,11 +134,12 @@ impl Window {
         ui.set_size(&protocol_states.compositor, size);
 
         let mut window = Self {
+            engine_viewport,
+            engine_surface,
             wayland_queue,
             egl_display,
             connection,
             active_tab,
-            viewport,
             tabs_ui,
             queue,
             size,
@@ -157,7 +157,7 @@ impl Window {
         };
 
         // Make sure primary window properties are set.
-        window.update_primary_surface(&protocol_states.compositor);
+        window.update_engine_surface(&protocol_states.compositor);
 
         // Create initial browser tab.
         window.add_tab(true)?;
@@ -270,7 +270,6 @@ impl Window {
         self.stalled = true;
 
         // Redraw the active browser engine.
-        let surface = self.xdg.wl_surface();
         let tabs_ui_visible = self.tabs_ui.visible();
         if !tabs_ui_visible {
             let engine_size: Size<i32> = self.engine_size().into();
@@ -279,9 +278,13 @@ impl Window {
             // Render buffer if one is attached and requires redraw.
             let dirty = engine.dirty() || self.dirty;
             if let Some(engine_buffer) = engine.wl_buffer().filter(|_| dirty) {
+                // Update browser's viewporter logical render size.
+                self.engine_viewport.set_destination(engine_size.width, engine_size.height);
+
                 // Attach engine buffer to primary surface.
-                surface.attach(Some(engine_buffer), 0, 0);
-                surface.damage(0, 0, engine_size.width, engine_size.height);
+                self.engine_surface.attach(Some(engine_buffer), 0, 0);
+                self.engine_surface.damage(0, 0, engine_size.width, engine_size.height);
+                self.engine_surface.commit();
 
                 // Request new engine frame.
                 engine.frame_done();
@@ -306,6 +309,7 @@ impl Window {
         }
 
         // Request a new frame if this frame was dirty.
+        let surface = self.xdg.wl_surface();
         if !self.stalled {
             surface.frame(&self.wayland_queue, surface.clone());
         }
@@ -361,7 +365,7 @@ impl Window {
         for engine in self.tabs.values_mut() {
             engine.set_size(engine_size);
         }
-        self.update_primary_surface(compositor);
+        self.update_engine_surface(compositor);
 
         // Resize UI element surface.
         self.tabs_ui.set_size(compositor, self.size);
@@ -448,7 +452,7 @@ impl Window {
         vertical: AxisScroll,
         modifiers: Modifiers,
     ) {
-        if self.xdg.wl_surface() == surface {
+        if &self.engine_surface == surface {
             // Forward event to browser engine.
             if let Some(engine) = self.tabs.get_mut(&self.active_tab) {
                 engine.pointer_axis(time, position, horizontal, vertical, modifiers);
@@ -467,7 +471,7 @@ impl Window {
         modifiers: Modifiers,
     ) {
         // Forward emulated touch event to UI.
-        if self.xdg.wl_surface() == surface {
+        if &self.engine_surface == surface {
             // Clear UI keyboard focus.
             self.ui.clear_keyboard_focus();
 
@@ -513,7 +517,7 @@ impl Window {
         modifiers: Modifiers,
     ) {
         // Forward events to corresponding surface.
-        if self.xdg.wl_surface() == surface {
+        if &self.engine_surface == surface {
             if let Some(engine) = self.tabs.get_mut(&self.active_tab) {
                 engine.pointer_motion(time, position, modifiers);
             }
@@ -541,7 +545,7 @@ impl Window {
         self.touch_points.insert(id, position);
 
         // Forward events to corresponding surface.
-        if self.xdg.wl_surface() == surface {
+        if &self.engine_surface == surface {
             if let Some(engine) = self.tabs.get_mut(&self.active_tab) {
                 // Clear UI keyboard focus.
                 self.ui.clear_keyboard_focus();
@@ -566,7 +570,7 @@ impl Window {
     /// Handle touch release events.
     pub fn touch_up(&mut self, surface: &WlSurface, time: u32, id: i32, modifiers: Modifiers) {
         // Forward events to corresponding surface.
-        if self.xdg.wl_surface() == surface {
+        if &self.engine_surface == surface {
             if let Some(engine) = self.tabs.get_mut(&self.active_tab) {
                 engine.touch_up(&self.touch_points, time, id, modifiers);
             }
@@ -597,7 +601,7 @@ impl Window {
         self.touch_points.insert(id, position);
 
         // Forward events to corresponding surface.
-        if self.xdg.wl_surface() == surface {
+        if &self.engine_surface == surface {
             if let Some(engine) = self.tabs.get_mut(&self.active_tab) {
                 engine.touch_motion(&self.touch_points, time, id, modifiers);
             }
@@ -673,7 +677,7 @@ impl Window {
 
     /// Check whether a surface is owned by this window.
     pub fn owns_surface(&self, surface: &WlSurface) -> bool {
-        self.xdg.wl_surface() == surface
+        &self.engine_surface == surface
             || self.ui.surface() == surface
             || self.tabs_ui.surface() == surface
     }
@@ -689,16 +693,12 @@ impl Window {
     }
 
     /// Update primary surface attributes.
-    fn update_primary_surface(&self, compositor: &CompositorState) {
-        let engine_size = self.engine_size();
-
-        // Update browser's viewporter logical render size.
-        self.viewport.set_destination(engine_size.width as i32, engine_size.height as i32);
-
+    fn update_engine_surface(&self, compositor: &CompositorState) {
         // Update opaque region.
         if let Ok(region) = Region::new(compositor) {
-            region.add(0, 0, engine_size.width as i32, engine_size.height as i32);
-            self.xdg.wl_surface().set_opaque_region(Some(region.wl_region()));
+            let engine_size: Size<i32> = self.engine_size().into();
+            region.add(0, 0, engine_size.width, engine_size.height);
+            self.engine_surface.set_opaque_region(Some(region.wl_region()));
         }
     }
 
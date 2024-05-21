catacombing/kumo

commit b813f1fabdaccba9b375d6156cea360c133d2046

Add IME support to browser engine
Previously the Wayland text-input protocol was only hooked up to the
text input in the UI. This patch extends this to the browser engine
using WebKit's `set_input_method_context` function.

This provides both the ability to submit text through IME and
automatically show/hide the input method. Preedit is still not supported
in the engine, since WebKit doesn't provide a way to submit preedit
text.

Closes #21.

src/engine/webkit/mod.rs
@@ -6,6 +6,7 @@ use std::sync::Once;
 use std::time::UNIX_EPOCH;
 
 use funq::StQueueHandle;
+use glib::object::ObjectExt;
 use glutin::api::egl::Egl;
 use glutin::display::{AsRawDisplay, Display, RawDisplay};
 use smithay_client_toolkit::reexports::client::protocol::wl_buffer::WlBuffer;
@@ -28,19 +29,25 @@ use wpe_backend_fdo_sys::{
     wpe_input_touch_event_raw, wpe_input_touch_event_type,
     wpe_input_touch_event_type_wpe_input_touch_event_type_down,
     wpe_input_touch_event_type_wpe_input_touch_event_type_motion,
-    wpe_input_touch_event_type_wpe_input_touch_event_type_up, wpe_view_backend_dispatch_axis_event,
-    wpe_view_backend_dispatch_keyboard_event, wpe_view_backend_dispatch_pointer_event,
-    wpe_view_backend_dispatch_set_device_scale_factor, wpe_view_backend_dispatch_set_size,
-    wpe_view_backend_dispatch_touch_event, wpe_view_backend_exportable_fdo,
-    wpe_view_backend_exportable_fdo_dispatch_frame_complete,
+    wpe_input_touch_event_type_wpe_input_touch_event_type_up,
+    wpe_view_activity_state_wpe_view_activity_state_focused,
+    wpe_view_activity_state_wpe_view_activity_state_visible, wpe_view_backend_add_activity_state,
+    wpe_view_backend_dispatch_axis_event, wpe_view_backend_dispatch_keyboard_event,
+    wpe_view_backend_dispatch_pointer_event, wpe_view_backend_dispatch_set_device_scale_factor,
+    wpe_view_backend_dispatch_set_size, wpe_view_backend_dispatch_touch_event,
+    wpe_view_backend_exportable_fdo, wpe_view_backend_exportable_fdo_dispatch_frame_complete,
     wpe_view_backend_exportable_fdo_egl_client, wpe_view_backend_exportable_fdo_egl_create,
     wpe_view_backend_exportable_fdo_egl_dispatch_release_exported_image,
     wpe_view_backend_exportable_fdo_get_view_backend,
 };
 use wpe_webkit::{Color, WebView, WebViewBackend, WebViewExt};
 
+use crate::engine::webkit::input_method_context::InputMethodContext;
 use crate::engine::{Engine, EngineId, BG};
+use crate::window::TextInputChange;
 use crate::{Position, Size, State};
+
+mod input_method_context;
 
 // Once for calling FDO initialization methods.
 static FDO_INIT: Once = Once::new();
@@ -120,6 +127,7 @@ impl WebKitHandler for State {
 
 /// WebKit browser engine.
 pub struct WebKitEngine {
+    input_method_context: InputMethodContext,
     backend: WebViewBackend,
     web_view: WebView,
 
@@ -195,11 +203,16 @@ impl WebKitEngine {
             queue.clone().set_display_uri(engine_id, uri);
         });
 
+        // Setup input handler.
+        let input_method_context = InputMethodContext::new();
+        web_view.set_input_method_context(Some(&input_method_context));
+
         // Get access to the OpenGL API.
         let Display::Egl(egl_display) = display;
         let egl = egl_display.egl();
 
         let mut engine = Self {
+            input_method_context,
             exportable,
             web_view,
             backend,
@@ -292,6 +305,22 @@ impl WebKitEngine {
         unsafe {
             let wpe_backend = self.backend.wpe_backend();
             wpe_view_backend_dispatch_touch_event(wpe_backend, &mut event);
+        }
+    }
+
+    fn set_focused(&mut self, focused: bool) {
+        // Force text-input update.
+        self.input_method_context.mark_text_input_dirty();
+
+        let state = if focused {
+            wpe_view_activity_state_wpe_view_activity_state_focused
+        } else {
+            wpe_view_activity_state_wpe_view_activity_state_visible
+        };
+
+        unsafe {
+            let backend = self.backend.wpe_backend();
+            wpe_view_backend_add_activity_state(backend, state);
         }
     }
 }
@@ -393,6 +422,8 @@ impl Engine for WebKitEngine {
         state: u32,
         modifiers: Modifiers,
     ) {
+        self.set_focused(true);
+
         self.pointer_button = button;
         self.pointer_state = state;
 
@@ -438,6 +469,8 @@ impl Engine for WebKitEngine {
         id: i32,
         modifiers: Modifiers,
     ) {
+        self.set_focused(true);
+
         let event_type = wpe_input_touch_event_type_wpe_input_touch_event_type_down;
         let touch_points = wpe_touch_points(touch_points, self.scale, time, id, event_type);
         self.touch_event(&touch_points, time, id, modifiers, event_type);
@@ -481,6 +514,29 @@ impl Engine for WebKitEngine {
 
     fn title(&self) -> String {
         self.web_view.title().unwrap_or_default().into()
+    }
+
+    fn text_input_state(&self) -> TextInputChange {
+        self.input_method_context.text_input_state()
+    }
+
+    fn delete_surrounding_text(&mut self, before_length: u32, after_length: u32) {
+        self.input_method_context
+            .emit_by_name::<()>("delete-surrounding", &[&before_length, &after_length]);
+    }
+
+    fn commit_string(&mut self, text: String) {
+        self.input_method_context.emit_by_name::<()>("committed", &[&text]);
+    }
+
+    fn preedit_string(&mut self, _text: String, _cursor_begin: i32, _cursor_end: i32) {
+        // NOTE: WebKit supports signaling preedit start/change/finish, but
+        // doesn't support forwarding the preedit text itself.
+    }
+
+    fn clear_focus(&mut self) {
+        // TODO: This makes engine transparent for some reason.
+        self.set_focused(false);
     }
 
     fn as_any(&mut self) -> &mut dyn Any {

src/engine/mod.rs
@@ -6,6 +6,7 @@ use smithay_client_toolkit::reexports::client::protocol::wl_buffer::WlBuffer;
 use smithay_client_toolkit::seat::keyboard::{Keysym, Modifiers};
 use smithay_client_toolkit::seat::pointer::AxisScroll;
 
+use crate::window::TextInputChange;
 use crate::{Position, Size, WindowId};
 
 pub mod webkit;
@@ -99,6 +100,21 @@ pub trait Engine {
 
     /// Get tab title.
     fn title(&self) -> String;
+
+    /// Get IME text_input state.
+    fn text_input_state(&self) -> TextInputChange;
+
+    /// Delete text around the current cursor position.
+    fn delete_surrounding_text(&mut self, before_length: u32, after_length: u32);
+
+    /// Insert text at the current cursor position.
+    fn commit_string(&mut self, text: String);
+
+    /// Set preedit text at the current cursor position.
+    fn preedit_string(&mut self, text: String, cursor_begin: i32, cursor_end: i32);
+
+    /// Clear engine focus.
+    fn clear_focus(&mut self);
 
     fn as_any(&mut self) -> &mut dyn Any;
 }

src/ui/mod.rs
@@ -16,7 +16,7 @@ use smithay_client_toolkit::reexports::protocols::wp::viewporter::client::wp_vie
 use smithay_client_toolkit::seat::keyboard::{Keysym, Modifiers};
 
 use crate::ui::renderer::{Renderer, TextOptions, Texture, TextureBuilder};
-use crate::window::TextInput;
+use crate::window::{TextInputChange, TextInputState};
 use crate::{gl, rect_contains, Position, Size, State, WindowId};
 
 mod renderer;
@@ -372,15 +372,14 @@ impl Ui {
         }
     }
 
-    /// Send current IME state to the compositor.
-    pub fn commit_ime_state(&mut self, text_input: &mut TextInput) {
-        // Update IME state or disable it if no text field is focused.
+    /// Get current IME text_input state.
+    pub fn text_input_state(&mut self) -> TextInputChange {
         match self.keyboard_focus {
             Some(KeyboardInputElement::UriBar) => {
                 let uribar_pos = self.uribar_position();
-                self.uribar.text_field.commit_ime_state(text_input, uribar_pos);
+                self.uribar.text_field.text_input_state(uribar_pos)
             },
-            _ => text_input.disable(),
+            _ => TextInputChange::Disabled,
         }
     }
 
@@ -744,12 +743,12 @@ struct TextField {
     preedit: (String, i32, i32),
     last_touch: TouchHistory,
     change_cause: ChangeCause,
-    last_ime_state: ImeState,
     purpose: ContentPurpose,
     layout: Layout,
     cursor_index: i32,
     cursor_offset: i32,
     focused: bool,
+    text_input_dirty: bool,
     dirty: bool,
 }
 
@@ -765,7 +764,7 @@ impl TextField {
             submit_handler: Box::new(|_| {}),
             change_cause: ChangeCause::Other,
             purpose: ContentPurpose::Normal,
-            last_ime_state: Default::default(),
+            text_input_dirty: Default::default(),
             cursor_offset: Default::default(),
             cursor_index: Default::default(),
             last_touch: Default::default(),
@@ -799,6 +798,7 @@ impl TextField {
         // Clear selection.
         self.selection = None;
 
+        self.text_input_dirty = true;
         self.dirty = true;
     }
 
@@ -832,6 +832,8 @@ impl TextField {
 
         if start < end {
             self.selection = Some(start..end);
+
+            self.text_input_dirty = true;
             self.dirty = true;
         } else {
             self.clear_selection();
@@ -841,6 +843,8 @@ impl TextField {
     /// Clear text selection.
     pub fn clear_selection(&mut self) {
         self.selection = None;
+
+        self.text_input_dirty = true;
         self.dirty = true;
     }
 
@@ -866,6 +870,8 @@ impl TextField {
                     },
                     None => self.move_cursor(-1),
                 }
+
+                self.text_input_dirty = true;
                 self.dirty = true;
             },
             (Keysym::Right, false, false) => {
@@ -882,6 +888,8 @@ impl TextField {
                     },
                     None => self.move_cursor(1),
                 }
+
+                self.text_input_dirty = true;
                 self.dirty = true;
             },
             (Keysym::BackSpace, false, false) => {
@@ -902,6 +910,7 @@ impl TextField {
                     },
                 }
 
+                self.text_input_dirty = true;
                 self.dirty = true;
             },
             (Keysym::Delete, false, false) => {
@@ -931,6 +940,7 @@ impl TextField {
                     },
                 }
 
+                self.text_input_dirty = true;
                 self.dirty = true;
             },
             (keysym, _, false) => {
@@ -949,6 +959,7 @@ impl TextField {
                     // Move cursor behind the new character.
                     self.move_cursor(1);
 
+                    self.text_input_dirty = true;
                     self.dirty = true;
                 }
             },
@@ -974,6 +985,9 @@ impl TextField {
             self.cursor_index = selection.start;
             self.cursor_offset = 0;
         }
+
+        self.text_input_dirty = true;
+        self.dirty = true;
     }
 
     /// Handle touch press events.
@@ -1008,6 +1022,9 @@ impl TextField {
 
                     // Clear selection.
                     self.selection = None;
+
+                    self.text_input_dirty = true;
+                    self.dirty = true;
                 }
             },
             1 => {
@@ -1035,8 +1052,6 @@ impl TextField {
 
         // Ensure focus when receiving touch input.
         self.set_focus(true);
-
-        self.dirty = true;
     }
 
     /// Handle touch motion events.
@@ -1072,6 +1087,7 @@ impl TextField {
             );
         }
 
+        self.text_input_dirty = true;
         self.dirty = true;
     }
 
@@ -1095,6 +1111,7 @@ impl TextField {
         // Set reason for next IME update.
         self.change_cause = ChangeCause::InputMethod;
 
+        self.text_input_dirty = true;
         self.dirty = true;
     }
 
@@ -1117,6 +1134,7 @@ impl TextField {
         // Set reason for next IME update.
         self.change_cause = ChangeCause::InputMethod;
 
+        self.text_input_dirty = true;
         self.dirty = true;
     }
 
@@ -1130,65 +1148,53 @@ impl TextField {
         }
 
         self.preedit = (text, cursor_begin, cursor_end);
+
+        self.text_input_dirty = true;
         self.dirty = true;
     }
 
-    /// Send current IME state to the compositor.
-    fn commit_ime_state(&mut self, text_input: &mut TextInput, position: Position) {
-        // Only send disable event if input is not focused.
+    /// Get current IME text_input state.
+    fn text_input_state(&mut self, position: Position) -> TextInputChange {
+        // Send disabled if input is not focused.
         if !self.focused {
-            text_input.disable();
-            return;
+            return TextInputChange::Disabled;
         }
 
-        // Skip if nothing has changed.
+        // Skip expensive surrounding_text clone without changes.
+        if !mem::take(&mut self.text_input_dirty) {
+            return TextInputChange::Unchanged;
+        }
+
+        // Get reason for this change.
+        let change_cause = mem::replace(&mut self.change_cause, ChangeCause::Other);
+
+        // Calculate cursor rectangle.
         let cursor_index = self.cursor_index();
-        let surrounding_text = self.text();
-        let ime_state = ImeState {
-            cursor_index,
-            surrounding_text: surrounding_text.clone(),
-            selection: self.selection.clone(),
-            purpose: self.purpose,
-        };
-        if text_input.enabled() && self.last_ime_state == ime_state {
-            return;
-        }
-        self.last_ime_state = ime_state;
-
-        // Enable IME if necessary.
-        text_input.enable();
-
-        // Offer the entire input text as surrounding text hint.
-        //
-        // NOTE: This request is technically limited to 4000 bytes, but that is unlikely
-        // to be an issue for our purposes.
-        let (cursor_index, selection_anchor) = match &self.selection {
-            Some(selection) => (selection.end, selection.start),
-            None => (cursor_index, cursor_index),
-        };
-        text_input.set_surrounding_text(surrounding_text, cursor_index, selection_anchor);
-
-        // Set reason for this update.
-        let cause = mem::replace(&mut self.change_cause, ChangeCause::Other);
-        text_input.set_text_change_cause(cause);
-
-        // Set text input field type.
-        text_input.set_content_type(ContentHint::None, self.purpose);
-
-        // Set cursor position.
         let (cursor_rect, _) = self.layout.cursor_pos(self.cursor_index());
         let cursor_x = position.x + cursor_rect.x() / PANGO_SCALE;
         let cursor_y = position.y + cursor_rect.y() / PANGO_SCALE;
         let cursor_height = cursor_rect.height() / PANGO_SCALE;
         let cursor_width = cursor_rect.width() / PANGO_SCALE;
-        text_input.set_cursor_rectangle(cursor_x, cursor_y, cursor_width, cursor_height);
+        let cursor_rect = (cursor_x, cursor_y, cursor_width, cursor_height);
 
-        text_input.commit();
+        // Skip if nothing has changed.
+        let surrounding_text = self.text();
+        TextInputChange::Dirty(TextInputState {
+            change_cause,
+            cursor_index,
+            cursor_rect,
+            surrounding_text: surrounding_text.clone(),
+            selection: self.selection.clone(),
+            hint: ContentHint::None,
+            purpose: self.purpose,
+        })
     }
 
     /// Set IME input field purpose hint.
     fn set_purpose(&mut self, purpose: ContentPurpose) {
         self.purpose = purpose;
+
+        self.text_input_dirty = true;
     }
 
     /// Set input focus.
@@ -1201,6 +1207,9 @@ impl TextField {
         }
 
         self.focused = focused;
+
+        self.text_input_dirty = true;
+        self.dirty = true;
     }
 
     /// Move the text input cursor.
@@ -1221,6 +1230,9 @@ impl TextField {
                 break;
             }
         }
+
+        self.text_input_dirty = true;
+        self.dirty = true;
     }
 
     /// Get current cursor's byte offset.

src/wayland/protocols/mod.rs
@@ -55,7 +55,6 @@ pub struct ProtocolStates {
 
 impl ProtocolStates {
     pub fn new(globals: &GlobalList, queue: &QueueHandle<State>) -> Self {
-        // SPB is optional for rendering a fallback when the engine is too slow.
         let text_input = TextInputManager::new(globals, queue);
         let registry = RegistryState::new(globals);
         let compositor = CompositorState::bind(globals, queue).unwrap();

src/engine/webkit/input_method_context.rs
@@ -1,0 +1,162 @@
+//! IME handler.
+
+use glib::subclass::types::ObjectSubclassIsExt;
+use glib::Object;
+
+use crate::window::TextInputChange;
+
+mod imp {
+    use std::cell::Cell;
+    use std::sync::atomic::{AtomicBool, Ordering};
+
+    use _text_input::zwp_text_input_v3::{ContentHint, ContentPurpose};
+    use glib::object::Cast;
+    use glib::subclass::prelude::*;
+    use smithay_client_toolkit::reexports::protocols::wp::text_input::zv3::client as _text_input;
+    use wpe_webkit::{InputHints, InputMethodContextExt, InputMethodContextImpl, InputPurpose};
+
+    use crate::window::{TextInputChange, TextInputState};
+
+    #[derive(Default)]
+    pub struct InputMethodContext {
+        text_input_state: Cell<Option<TextInputState>>,
+        dirty: AtomicBool,
+    }
+
+    impl InputMethodContext {
+        /// Get current IME text_input state.
+        pub fn text_input_state(&self) -> TextInputChange {
+            // Skip expensive surrounding_text clone without changes.
+            if !self.dirty.fetch_and(false, Ordering::Relaxed) {
+                return TextInputChange::Unchanged;
+            }
+
+            let mut text_input_state = match self.text_input_state.take() {
+                Some(text_input_state) => text_input_state,
+                None => return TextInputChange::Disabled,
+            };
+            self.text_input_state.set(Some(text_input_state.clone()));
+
+            // Map WebKit purpose to Wayland purpose.
+            let context_obj = self.obj();
+            let webkit_context =
+                unsafe { context_obj.unsafe_cast_ref::<wpe_webkit::InputMethodContext>() };
+            text_input_state.purpose = match webkit_context.input_purpose() {
+                InputPurpose::Digits => ContentPurpose::Digits,
+                InputPurpose::Number => ContentPurpose::Number,
+                InputPurpose::Phone => ContentPurpose::Phone,
+                InputPurpose::Url => ContentPurpose::Url,
+                InputPurpose::Email => ContentPurpose::Email,
+                InputPurpose::Password => ContentPurpose::Password,
+                _ => ContentPurpose::Normal,
+            };
+
+            // Map WebKit hint to Wayland hint.
+            let wpe_hints = webkit_context.input_hints();
+            let mut hint = ContentHint::None;
+            hint.set(ContentHint::Spellcheck, wpe_hints.contains(InputHints::SPELLCHECK));
+            hint.set(ContentHint::Lowercase, wpe_hints.contains(InputHints::LOWERCASE));
+            let wpe_uppercase = InputHints::UPPERCASE_CHARS
+                | InputHints::UPPERCASE_WORDS
+                | InputHints::UPPERCASE_SENTENCES;
+            hint.set(ContentHint::Uppercase, wpe_hints.contains(wpe_uppercase));
+            text_input_state.hint = hint;
+
+            TextInputChange::Dirty(text_input_state)
+        }
+
+        /// Mark text input state as dirty.
+        ///
+        /// This is useful to force a resubmission when switching between
+        /// surfaces.
+        pub fn mark_dirty(&self) {
+            self.dirty.store(true, Ordering::Relaxed);
+        }
+    }
+
+    impl InputMethodContextImpl for InputMethodContext {
+        fn notify_focus_in(&self) {
+            self.text_input_state.set(Some(TextInputState::default()));
+
+            self.dirty.store(true, Ordering::Relaxed);
+        }
+
+        fn notify_focus_out(&self) {
+            self.text_input_state.take();
+
+            self.dirty.store(true, Ordering::Relaxed);
+        }
+
+        fn notify_cursor_area(&self, x: i32, y: i32, width: i32, height: i32) {
+            if let Some(mut text_input_state) = self.text_input_state.take() {
+                text_input_state.cursor_rect = (x, y, width, height);
+                self.text_input_state.set(Some(text_input_state));
+
+                self.dirty.store(true, Ordering::Relaxed);
+            }
+        }
+
+        fn notify_surrounding(&self, text: &str, cursor_index: u32, selection_index: u32) {
+            let selection_index = selection_index as i32;
+            let cursor_index = cursor_index as i32;
+            if let Some(mut text_input_state) = self.text_input_state.take() {
+                text_input_state.surrounding_text = text.into();
+                text_input_state.cursor_index = cursor_index;
+                if selection_index != cursor_index {
+                    text_input_state.selection = Some(cursor_index..selection_index);
+                }
+                self.text_input_state.set(Some(text_input_state));
+
+                self.dirty.store(true, Ordering::Relaxed);
+            }
+        }
+
+        fn reset(&self) {
+            if self.text_input_state.take().is_some() {
+                self.text_input_state.set(Some(TextInputState::default()));
+
+                self.dirty.store(true, Ordering::Relaxed);
+            }
+        }
+    }
+
+    impl ObjectImpl for InputMethodContext {}
+
+    #[glib::object_subclass]
+    impl ObjectSubclass for InputMethodContext {
+        type ParentType = wpe_webkit::InputMethodContext;
+        type Type = super::InputMethodContext;
+
+        const NAME: &'static str = "WebkitInputMethodContext";
+    }
+}
+
+glib::wrapper! {
+    pub struct InputMethodContext(ObjectSubclass<imp::InputMethodContext>)
+        @extends wpe_webkit::InputMethodContext;
+}
+
+impl InputMethodContext {
+    pub fn new() -> Self {
+        Object::new()
+    }
+
+    /// Get current IME text_input state.
+    pub fn text_input_state(&self) -> TextInputChange {
+        self.imp().text_input_state()
+    }
+
+    /// Mark text input state as dirty.
+    ///
+    /// This is useful to force a resubmission when switching between
+    /// surfaces.
+    pub fn mark_text_input_dirty(&self) {
+        self.imp().mark_dirty()
+    }
+}
+
+impl Default for InputMethodContext {
+    fn default() -> Self {
+        Self::new()
+    }
+}

src/main.rs
@@ -62,7 +62,7 @@ fn main() -> Result<(), Error> {
         if i > 0 {
             window.add_tab(false)?;
         } else {
-            window.clear_keyboard_focus();
+            window.clear_focus();
         }
         window.load_uri(arg);
     }

commit ffecaa0237c0a10478692f9f295db7e24db4fbc3

Fix WPE activity state changes
This fixes an issue with the `set_focus` call introduced in b813f1f,
which updated the activity state to indicate focus but caused the engine
to assume the window wasn't focused.

This patch sets the activity state at startup to include all activity
states, then toggles the focus state whenever `set_focus` is called.

src/engine/webkit/mod.rs
@@ -31,6 +31,7 @@ use wpe_backend_fdo_sys::{
     wpe_input_touch_event_type_wpe_input_touch_event_type_motion,
     wpe_input_touch_event_type_wpe_input_touch_event_type_up,
     wpe_view_activity_state_wpe_view_activity_state_focused,
+    wpe_view_activity_state_wpe_view_activity_state_in_window,
     wpe_view_activity_state_wpe_view_activity_state_visible, wpe_view_backend_add_activity_state,
     wpe_view_backend_dispatch_axis_event, wpe_view_backend_dispatch_keyboard_event,
     wpe_view_backend_dispatch_pointer_event, wpe_view_backend_dispatch_set_device_scale_factor,
@@ -38,7 +39,7 @@ use wpe_backend_fdo_sys::{
     wpe_view_backend_exportable_fdo, wpe_view_backend_exportable_fdo_dispatch_frame_complete,
     wpe_view_backend_exportable_fdo_egl_client, wpe_view_backend_exportable_fdo_egl_create,
     wpe_view_backend_exportable_fdo_egl_dispatch_release_exported_image,
-    wpe_view_backend_exportable_fdo_get_view_backend,
+    wpe_view_backend_exportable_fdo_get_view_backend, wpe_view_backend_remove_activity_state,
 };
 use wpe_webkit::{Color, WebView, WebViewBackend, WebViewExt};
 
@@ -207,6 +208,14 @@ impl WebKitEngine {
         let input_method_context = InputMethodContext::new();
         web_view.set_input_method_context(Some(&input_method_context));
 
+        // Set initial activity state.
+        let state = wpe_view_activity_state_wpe_view_activity_state_visible
+            | wpe_view_activity_state_wpe_view_activity_state_in_window
+            | wpe_view_activity_state_wpe_view_activity_state_focused;
+        unsafe {
+            wpe_view_backend_add_activity_state(backend.wpe_backend(), state);
+        }
+
         // Get access to the OpenGL API.
         let Display::Egl(egl_display) = display;
         let egl = egl_display.egl();
@@ -308,19 +317,19 @@ impl WebKitEngine {
         }
     }
 
+    /// Update engine focus.
     fn set_focused(&mut self, focused: bool) {
         // Force text-input update.
         self.input_method_context.mark_text_input_dirty();
 
-        let state = if focused {
-            wpe_view_activity_state_wpe_view_activity_state_focused
-        } else {
-            wpe_view_activity_state_wpe_view_activity_state_visible
-        };
-
+        let state = wpe_view_activity_state_wpe_view_activity_state_focused;
         unsafe {
             let backend = self.backend.wpe_backend();
-            wpe_view_backend_add_activity_state(backend, state);
+            if focused {
+                wpe_view_backend_add_activity_state(backend, state);
+            } else {
+                wpe_view_backend_remove_activity_state(backend, state);
+            }
         }
     }
 }
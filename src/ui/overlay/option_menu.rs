catacombing/kumo

commit bdcf5d418a39442d722f58cfd61df8885719b627

Optimize history menu rendering
This patch makes several optimizations to improve the performance of
opening and modifying the history menu popup.

Since d536a76 has introduced a cheap way to calculate item height
without rasterization, it was made possible to skip rendering of any
option menu item that is fully invisible.

The history menu specifically also has the property of often showing
very similar entries due to the minimal changes generally made during
text input. To optimize for this, a function to update the items in an
option menu has been introduced. This new mechanism will rely on a cache
of the last 100 items to avoid re-creating items that have recently been
seen in the menu.

Lastly, a function to show and hide option menus was introduced. The
history menu previously was frequently closed whenever there were no
suggestions, which would clear the cache introduced in this patch. To
keep the cache around, the history menu is now reused and its visibility
is used to show/hide it when necessary.

This also fixes a minor bug where `OptionMenu::scroll` would set
dirtiness to the result of `self.scroll_offset != scroll_offset`,
instead of assigning it only when `true`.

Closes #42.

src/ui/overlay/option_menu.rs
@@ -79,6 +79,7 @@ impl OptionMenuHandler for State {
 pub struct OptionMenu {
     id: OptionMenuId,
     items: Vec<OptionMenuRenderItem>,
+    item_cache: Vec<OptionMenuRenderItem>,
     selection_index: Option<usize>,
 
     queue: MtQueueHandle<State>,
@@ -94,6 +95,7 @@ pub struct OptionMenu {
     borders: Borders,
     border: Option<Texture>,
 
+    visible: bool,
     dirty: bool,
 }
 
@@ -118,9 +120,11 @@ impl OptionMenu {
             id,
             borders: Borders::all(),
             width: item_width,
+            visible: true,
             selection_index: Default::default(),
             scroll_offset: Default::default(),
             touch_state: Default::default(),
+            item_cache: Default::default(),
             max_height: Default::default(),
             border: Default::default(),
             items: Default::default(),
@@ -146,9 +150,72 @@ impl OptionMenu {
         menu
     }
 
+    /// Update the items in this menu.
+    #[cfg_attr(feature = "profiling", profiling::function)]
+    pub fn set_items<I>(&mut self, items: I)
+    where
+        I: Iterator<Item = OptionMenuItem>,
+    {
+        /// Maximum number of items kept for caching purposes.
+        const MAX_CACHED_ITEMS: usize = 100;
+
+        // Add all existing entries to the beginning of the cache.
+        self.items.truncate(MAX_CACHED_ITEMS);
+        let items_len = self.items.len();
+        self.item_cache.truncate(MAX_CACHED_ITEMS - items_len);
+        self.item_cache.append(&mut self.items);
+        self.item_cache.rotate_right(items_len);
+
+        // Ensure selection doesn't point out of bounds.
+        self.selection_index = None;
+
+        // Try to load items from cache and create new ones if necessary.
+        let item_width = self.item_width();
+        self.items = items
+            .enumerate()
+            .map(|(i, item)| {
+                // Update selected item.
+                if item.selected {
+                    self.selection_index = Some(i);
+                }
+
+                // Look for existing item with this label and description.
+                let existing_index = self
+                    .item_cache
+                    .iter()
+                    .position(|i| i.label == item.label && i.description == item.description);
+
+                match existing_index {
+                    // Update existing item, to cache pango layout.
+                    Some(index) => {
+                        let mut existing = self.item_cache.swap_remove(index);
+                        existing.set_disabled(item.disabled);
+                        existing
+                    },
+                    // Create a new item.
+                    None => OptionMenuRenderItem::new(item, item_width, self.scale),
+                }
+            })
+            .collect();
+
+        self.scroll_offset = 0.;
+        self.dirty = true;
+    }
+
     /// Get the popup's ID,
     pub fn id(&self) -> OptionMenuId {
         self.id
+    }
+
+    /// Set visibility of the option menu.
+    pub fn set_visible(&mut self, visible: bool) {
+        self.dirty |= visible && self.visible != visible;
+        self.visible = visible;
+    }
+
+    /// Get visibility of the option menu.
+    pub fn visible(&self) -> bool {
+        self.visible
     }
 
     /// Move scroll position.
@@ -157,7 +224,7 @@ impl OptionMenu {
         let scroll_offset = match target {
             ScrollTarget::End => self.max_scroll_offset() as f32,
         };
-        self.dirty = self.scroll_offset != scroll_offset;
+        self.dirty |= self.scroll_offset != scroll_offset;
         self.scroll_offset = scroll_offset;
     }
 
@@ -259,7 +326,27 @@ impl OptionMenu {
 
 impl Popup for OptionMenu {
     fn dirty(&self) -> bool {
-        self.dirty || self.items.iter().any(|item| item.dirty)
+        if self.dirty {
+            return true;
+        }
+
+        // Get Y position of first item.
+        let max_height = (self.max_height as f32 * self.scale as f32).round();
+        let start_position = self.position().y as f32 + self.border_widths().top as f32;
+        let mut y = start_position * self.scale as f32 - self.scroll_offset;
+
+        // Check visible textures for dirtiness.
+        for item in &self.items {
+            let height = item.height() as f32;
+            if y >= max_height {
+                break;
+            } else if item.dirty && y + height >= 0. {
+                return true;
+            }
+            y += height;
+        }
+
+        false
     }
 
     #[cfg_attr(feature = "profiling", profiling::function)]
@@ -296,16 +383,21 @@ impl Popup for OptionMenu {
         // Draw each option menu entry.
         let max_height = (self.max_height as f32 * self.scale as f32).round();
         for (i, item) in self.items.iter_mut().enumerate() {
-            // NOTE: This must be called on all textures to clear dirtiness flag.
-            let selected = self.selection_index == Some(i);
-            let texture = item.texture(selected);
-
             // Skip rendering out of bounds textures.
-            if position.y + texture.height as f32 >= 0. && position.y < max_height {
-                unsafe { renderer.draw_texture_at(texture, position, None) };
+            let height = item.height() as f32;
+            if position.y >= max_height {
+                break;
+            } else if position.y + height < 0. {
+                position.y += height;
+                continue;
             }
 
-            position.y += texture.height as f32;
+            // Create and draw the texture.
+            let selected = self.selection_index == Some(i);
+            let texture = item.texture(selected);
+            unsafe { renderer.draw_texture_at(texture, position, None) };
+
+            position.y += height;
         }
 
         // Disable scissoring again.
@@ -458,27 +550,34 @@ struct OptionMenuRenderItem {
     description_layout: Option<TextLayout>,
     /// Whether item is selectable.
     disabled: bool,
+
+    /// Text label used for layout creation.
+    label: String,
+    /// Text description used for layout creation.
+    description: String,
 }
 
 impl OptionMenuRenderItem {
     fn new(item: OptionMenuItem, item_width: u32, scale: f64) -> Self {
         // Create a new pango layout.
-        let create_layout = |text: String, font_size: u8| {
+        let create_layout = |text: &str, font_size: u8| {
             let layout = TextLayout::new(font_size, scale);
-            layout.set_text(&text);
+            layout.set_text(text);
             layout.set_height(0);
             layout
         };
 
         let description_layout = (!item.description.is_empty())
-            .then(|| create_layout(item.description, DESCRIPTION_FONT_SIZE));
-        let label_layout = create_layout(item.label, LABEL_FONT_SIZE);
+            .then(|| create_layout(&item.description, DESCRIPTION_FONT_SIZE));
+        let label_layout = create_layout(&item.label, LABEL_FONT_SIZE);
 
         OptionMenuRenderItem {
             description_layout,
             label_layout,
             scale,
+            description: item.description,
             disabled: item.disabled,
+            label: item.label,
             width: item_width,
             texture: None,
             dirty: true,
@@ -570,6 +669,12 @@ impl OptionMenuRenderItem {
         self.label_layout.set_scale(scale);
         self.scale = scale;
         self.dirty = true;
+    }
+
+    /// Update disabled status.
+    fn set_disabled(&mut self, disabled: bool) {
+        self.dirty |= self.disabled != disabled;
+        self.disabled = disabled;
     }
 }
 

src/window.rs
@@ -826,13 +826,11 @@ impl Window {
     /// Show history options menu.
     #[cfg_attr(feature = "profiling", profiling::function)]
     pub fn open_history_menu(&mut self, matches: SmallVec<[HistoryMatch; MAX_MATCHES]>) {
-        // Close old menu.
-        if let Some(menu_id) = self.history_menu.take() {
-            self.close_option_menu(menu_id);
-        }
-
         // Skip new menu creation without matches.
         if matches.is_empty() {
+            // Close old menu.
+            self.close_history_menu();
+
             return;
         }
 
@@ -847,20 +845,34 @@ impl Window {
             OptionMenuItem { label, description, disabled: false, selected: false }
         });
 
-        // Open new menu.
-        let position = Position::new(0, self.size.height as i32);
-        let menu_id = OptionMenuId::new(self.id);
-        let width = self.size.width;
-        let menu = self.overlay.open_option_menu(menu_id, position, width, self.scale, items);
-        menu.set_borders(Borders::TOP);
-        menu.scroll(ScrollTarget::End);
-        self.history_menu = Some(menu_id);
+        match self.history_menu.and_then(|id| self.overlay.option_menu(id)) {
+            // Update contents of existing menu.
+            Some(menu) => {
+                menu.set_visible(true);
+                menu.set_items(items);
+                menu.scroll(ScrollTarget::End);
+            },
+            // Open new menu.
+            None => {
+                let menu_id = OptionMenuId::new(self.id);
+                let menu = self.overlay.open_option_menu(
+                    menu_id,
+                    Position::new(0, self.size.height as i32),
+                    self.size.width,
+                    self.scale,
+                    items,
+                );
+                menu.set_borders(Borders::TOP);
+                menu.scroll(ScrollTarget::End);
+                self.history_menu = Some(menu_id);
+            },
+        }
     }
 
     /// Hide history options menu.
     pub fn close_history_menu(&mut self) {
-        if let Some(menu_id) = self.history_menu.take() {
-            self.overlay.close_option_menu(menu_id);
+        if let Some(menu) = self.history_menu.and_then(|id| self.overlay.option_menu(id)) {
+            menu.set_visible(false);
         }
     }
 

src/ui/mod.rs
@@ -1453,8 +1453,6 @@ impl TextField {
         self.dirty = true;
     }
 
-    // TODO: ~1ms mean time.
-    //
     /// Move the text input cursor.
     #[cfg_attr(feature = "profiling", profiling::function)]
     fn move_cursor(&mut self, positions: i32) {

src/ui/overlay/mod.rs
@@ -281,7 +281,12 @@ impl Overlay {
         self.popups.option_menus.last_mut().unwrap()
     }
 
-    /// Hide an option menu.
+    /// Get mutable reference to an option menu.
+    pub fn option_menu(&mut self, id: OptionMenuId) -> Option<&mut OptionMenu> {
+        self.popups.option_menus.iter_mut().find(|menu| menu.id() == id)
+    }
+
+    /// Permanently discard an option menu.
     pub fn close_option_menu(&mut self, id: OptionMenuId) {
         self.popups.option_menus.retain(|menu| menu.id() != id);
     }
@@ -318,20 +323,21 @@ impl Popups {
     /// Non-mutable popup iterator.
     fn iter(&self) -> Box<dyn Iterator<Item = &dyn Popup> + '_> {
         if self.tabs.visible() {
-            Box::new(self.option_menus.iter().map(|menu| menu as _).chain([&self.tabs as _]))
+            let option_menus = self.option_menus.iter().filter(|m| m.visible()).map(|m| m as _);
+            Box::new(option_menus.chain([&self.tabs as _]))
         } else {
-            Box::new(self.option_menus.iter().map(|menu| menu as _))
+            Box::new(self.option_menus.iter().filter(|menu| menu.visible()).map(|menu| menu as _))
         }
     }
 
     /// Mutable popup iterator.
     fn iter_mut(&mut self) -> Box<dyn Iterator<Item = &mut dyn Popup> + '_> {
         if self.tabs.visible() {
-            Box::new(
-                self.option_menus.iter_mut().map(|menu| menu as _).chain([&mut self.tabs as _]),
-            )
+            let option_menus = self.option_menus.iter_mut().filter(|m| m.visible()).map(|m| m as _);
+            Box::new(option_menus.chain([&mut self.tabs as _]))
         } else {
-            Box::new(self.option_menus.iter_mut().map(|menu| menu as _))
+            let iter = self.option_menus.iter_mut().filter(|menu| menu.visible()).map(|m| m as _);
+            Box::new(iter)
         }
     }
 }

src/ui/renderer.rs
@@ -314,6 +314,7 @@ pub struct Texture {
 
 impl Texture {
     /// Load a buffer as texture into OpenGL.
+    #[cfg_attr(feature = "profiling", profiling::function)]
     pub fn new(buffer: &[u8], width: usize, height: usize) -> Self {
         assert!(buffer.len() == width * height * 4);
 

commit 691cccd74fc66b5deb2cc0c721af94fabadfc9ba

Fix history menu scale changes
This fixes an issue where the history menu would keep around incorrectly
scaled items with an incorrect position after the scale of the window
changed.

The original code used `close_history_menu` in the `configure` handling
to avoid worrying about this, however that got broken in bdcf5d4 since
it changed all related code to just hide the menu temporarily.

Since performance shouldn't be a big factor for a scale change and a
full re-rasterization of all text is necessary anyway, the original
approach has been reimplemented by replacing the `close_history_menu`
call with a full destruction of the cached history menu.

src/ui/overlay/option_menu.rs
@@ -665,12 +665,16 @@ impl OptionMenuRenderItem {
 
     /// Update item width.
     fn set_width(&mut self, width: u32) {
+        self.dirty |= self.width != width;
         self.width = width;
-        self.dirty = true;
     }
 
     /// Update item scale.
     fn set_scale(&mut self, scale: f64) {
+        if self.scale == scale {
+            return;
+        }
+
         if let Some(layout) = &mut self.description_layout {
             layout.set_scale(scale);
         }

src/window.rs
@@ -466,8 +466,10 @@ impl Window {
             }
         }
 
-        // Close history popup, so we don't need to resize it.
-        self.close_history_menu();
+        // Destroy history popup, so we don't need to resize it.
+        if let Some(id) = self.history_menu {
+            self.overlay.close_option_menu(id);
+        }
 
         self.unstall();
     }
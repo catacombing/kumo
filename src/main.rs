catacombing/kumo

commit 288e8da94538e2a79ed25c28089b49f04bcada43

Add session recovery on startup
This patch uses the existing `history.sqlite` file to keep track of all
open browser sessions. When a new Kumo instance is opened, all inactive
sessions in the history are recovered. Recovery can only be prevented by
closing Kumo through closing every tab.

Session activity is tracked by storing the Kumo process ID owning each
session, then sending a `kill(pid, 0)` to see if that process is still
alive or not. This should work reliably as long as the `history.sqlite`
file is never used by multiple users at the same time.

Closes #46.

src/main.rs
@@ -1,11 +1,12 @@
 use std::cell::RefCell;
+use std::cmp::Ordering;
 use std::collections::HashMap;
 use std::ops::{Add, AddAssign, Mul, Sub, SubAssign};
 use std::os::fd::{AsFd, AsRawFd};
 use std::ptr::NonNull;
 use std::rc::Rc;
 use std::time::Duration;
-use std::{env, io};
+use std::{env, io, mem};
 
 use funq::{MtQueueHandle, Queue, StQueueHandle};
 use glib::{source, ControlFlow, IOCondition, MainLoop, Priority, Source};
@@ -82,14 +83,62 @@ fn main() -> Result<(), Error> {
     // Create our initial window.
     let window_id = state.create_window()?;
 
-    // Spawn a new tab for every CLI argument.
-    let window = state.windows.get_mut(&window_id).unwrap();
-    for (i, arg) in env::args().skip(1).enumerate() {
-        if i > 0 {
-            window.add_tab(false)?;
+    // Create an empty tab for loading a new page.
+    let get_empty_tab = |window: &mut Window| -> Result<_, Error> {
+        if window.tabs().len() > 1 {
+            Ok(window.add_tab(false)?)
         } else {
             window.set_keyboard_focus(KeyboardFocus::None);
+            Ok(window.active_tab())
         }
+    };
+
+    // Get all sessions requiring restoration, sorted by PID and window ID.
+    let mut orphan_sessions = state.history.orphan_sessions();
+    orphan_sessions.sort_by(|a, b| match a.pid.cmp(&b.pid) {
+        Ordering::Equal => a.window_id.cmp(&b.window_id),
+        ordering => ordering,
+    });
+
+    // Restore all orphan sessions.
+    let mut window = state.windows.get_mut(&window_id).unwrap();
+    let mut session_window_id = None;
+    let mut session_pid = None;
+    for entry in &mut orphan_sessions {
+        // Create new window if session's process or window changed.
+        if session_pid != Some(entry.pid) || session_window_id != Some(entry.window_id) {
+            // Create a new window.
+            if session_pid.is_some() || session_window_id.is_some() {
+                let new_window_id = state.create_window()?;
+                window = state.windows.get_mut(&new_window_id).unwrap();
+            }
+
+            session_window_id = Some(entry.window_id);
+            session_pid = Some(entry.pid);
+        }
+
+        // Restore the session in a new empty tab.
+        let engine_id = get_empty_tab(window)?;
+        if let Some(engine) = window.tabs().get(&engine_id) {
+            engine.restore_session(mem::take(&mut entry.session_data));
+            engine.load_uri(&entry.uri);
+        }
+    }
+
+    // Update database with the adopted sessions.
+    //
+    // NOTE: Calling `load_uri` automatically causes the session to be persisted
+    // once the page is loaded, however we explicitly sync here to avoid session
+    // loss due to a racing condition.
+    for window in state.windows.values() {
+        window.persist_session(&state.history);
+    }
+    state.history.delete_orphan_sessions(orphan_sessions.iter().map(|s| s.pid));
+
+    // Spawn a new tab for every CLI argument.
+    let window = state.windows.get_mut(&window_id).unwrap();
+    for arg in env::args().skip(1) {
+        get_empty_tab(window)?;
         window.load_uri(arg);
     }
 

src/engine/mod.rs
@@ -137,6 +137,12 @@ pub trait Engine {
     /// Notify engine about change to the fullscreen state.
     fn set_fullscreen(&mut self, fulscreened: bool);
 
+    /// Get a serialized version of the current session.
+    fn session(&self) -> Vec<u8>;
+
+    /// Restore a browser session.
+    fn restore_session(&self, session: Vec<u8>);
+
     fn as_any(&mut self) -> &mut dyn Any;
 }
 

src/engine/webkit/mod.rs
@@ -24,7 +24,7 @@ use wpe_platform::ffi::WPERectangle;
 use wpe_platform::{Buffer, BufferDMABuf, BufferExt, BufferSHM, EventType};
 use wpe_webkit::{
     Color, CookieAcceptPolicy, CookiePersistentStorage, NetworkSession, OptionMenu,
-    UserContentFilterStore, WebView, WebViewExt,
+    UserContentFilterStore, WebView, WebViewExt, WebViewSessionState,
 };
 
 use crate::engine::webkit::platform::WebKitDisplay;
@@ -284,7 +284,6 @@ impl WebKitEngine {
         let network_session = xdg_network_session().unwrap_or_else(NetworkSession::new_ephemeral);
         let web_view =
             WebView::builder().network_session(&network_session).display(&webkit_display).build();
-        web_view.load_uri("about:blank");
 
         // Set browser background color.
         let mut color = Color::new(BG[0], BG[1], BG[2], 1.);
@@ -611,6 +610,18 @@ impl Engine for WebKitEngine {
 
     fn set_fullscreen(&mut self, fullscreened: bool) {
         self.webkit_display.set_fullscreen(fullscreened);
+    }
+
+    fn session(&self) -> Vec<u8> {
+        self.web_view
+            .session_state()
+            .and_then(|session| session.serialize())
+            .map_or(Vec::new(), |session| session.to_vec())
+    }
+
+    fn restore_session(&self, session: Vec<u8>) {
+        let session = WebViewSessionState::new(&Bytes::from_owned(session));
+        self.web_view.restore_session_state(&session);
     }
 
     fn as_any(&mut self) -> &mut dyn Any {

src/history.rs
@@ -1,14 +1,17 @@
 //! Browser history.
 
 use std::collections::HashMap;
-use std::fs;
 use std::path::Path;
 use std::rc::Rc;
 use std::sync::RwLock;
+use std::{fs, process};
 
-use rusqlite::Connection as SqliteConnection;
+use rusqlite::{params, Connection as SqliteConnection};
 use smallvec::SmallVec;
 use tracing::error;
+
+use crate::engine::Engine;
+use crate::window::WindowId;
 
 /// Maximum scored history matches compared.
 pub const MAX_MATCHES: usize = 25;
@@ -18,14 +21,17 @@ pub const MAX_MATCHES: usize = 25;
 pub struct History {
     entries: Rc<RwLock<HashMap<HistoryUri, HistoryEntry>>>,
     db: Option<Rc<HistoryDb>>,
+    pid: u32,
 }
 
 impl History {
     pub fn new() -> Self {
+        let pid = process::id();
+
         // Get storage path, ignoring persistence if it can't be retrieved.
         let data_dir = match dirs::data_dir() {
             Some(data_dir) => data_dir.join("kumo/default/history.sqlite"),
-            None => return Self { entries: Default::default(), db: Default::default() },
+            None => return Self { pid, entries: Default::default(), db: Default::default() },
         };
 
         let (db, entries) = match HistoryDb::new(&data_dir) {
@@ -45,7 +51,7 @@ impl History {
             },
         };
 
-        Self { entries, db }
+        Self { entries, pid, db }
     }
 
     /// Increment URI visit count for history.
@@ -159,6 +165,65 @@ impl History {
 
         matches
     }
+
+    /// Update the browser session for a window.
+    #[cfg_attr(feature = "profiling", profiling::function)]
+    pub fn set_session(&self, window_id: WindowId, session: Vec<SessionRecord>) {
+        let db = match &self.db {
+            Some(db) => db,
+            None => return,
+        };
+
+        // Write sessions to database.
+        if let Err(err) = db.set_session(self.pid, window_id, session) {
+            error!("Failed session update: {err}");
+        }
+    }
+
+    /// Get all sessions not owned by any active process.
+    #[cfg_attr(feature = "profiling", profiling::function)]
+    pub fn orphan_sessions(&self) -> Vec<SessionEntry> {
+        let db = match &self.db {
+            Some(db) => db,
+            None => return Vec::new(),
+        };
+
+        // Get all sessions from the DB.
+        let mut sessions = db
+            .sessions()
+            .inspect_err(|err| error!("Failed sessions read: {err}"))
+            .unwrap_or_default();
+
+        // Remove sessions of currently active Kumo processes.
+        let mut known_pids = HashMap::new();
+        sessions.retain(|session| {
+            // Short-circuit if we've already probed the PID owning this session.
+            if let Some(running) = known_pids.get(&session.pid) {
+                return !running;
+            }
+
+            // Probe PID to check if the session is currently active.
+            let running = unsafe { libc::kill(session.pid as i32, 0) } >= 0;
+            known_pids.insert(session.pid, running);
+
+            !running
+        });
+
+        sessions
+    }
+
+    /// Delete sessions for orphan PIDs.
+    #[cfg_attr(feature = "profiling", profiling::function)]
+    pub fn delete_orphan_sessions(&self, pids: impl IntoIterator<Item = u32>) {
+        let db = match &self.db {
+            Some(db) => db,
+            None => return,
+        };
+
+        if let Err(err) = db.delete_sessions(pids) {
+            error!("Failed delete orphan sessions: {err}");
+        }
+    }
 }
 
 /// DB for persisting history data.
@@ -176,12 +241,23 @@ impl HistoryDb {
 
         let connection = SqliteConnection::open(path)?;
 
-        // Setup table if it doesn't exist yet.
+        // Setup history table if it doesn't exist yet.
         connection.execute(
             "CREATE TABLE IF NOT EXISTS history (
                 uri TEXT NOT NULL PRIMARY KEY,
                 title TEXT DEFAULT '',
                 views INTEGER NOT NULL DEFAULT 1
+            )",
+            [],
+        )?;
+
+        // Setup session table if it doesn't exist yet.
+        connection.execute(
+            "CREATE TABLE IF NOT EXISTS session (
+                pid INTEGER NOT NULL,
+                window_id INTEGER NOT NULL,
+                data BLOB NOT NULL,
+                uri TEXT NOT NULL
             )",
             [],
         )?;
@@ -222,6 +298,62 @@ impl HistoryDb {
     fn set_title(&self, uri: &str, title: &str) -> rusqlite::Result<()> {
         self.connection.execute("UPDATE history SET title=?1 WHERE uri=?2", [title, uri])?;
 
+        Ok(())
+    }
+
+    /// Update the active browser session for a window.
+    fn set_session(
+        &self,
+        pid: u32,
+        window_id: WindowId,
+        session: Vec<SessionRecord>,
+    ) -> rusqlite::Result<()> {
+        let tx = self.connection.unchecked_transaction()?;
+
+        // Delete old session.
+        let window_id = window_id.as_raw();
+        tx.execute("DELETE FROM session WHERE pid = ?1 AND window_id = ?2", params![
+            pid, window_id
+        ])?;
+
+        // Save current session.
+        if !session.is_empty() {
+            let mut stmt = tx.prepare(
+                "INSERT INTO session (pid, window_id, data, uri) VALUES (?1, ?2, ?3, ?4)",
+            )?;
+            for entry in session {
+                stmt.execute(params![pid, window_id, entry.data, entry.uri])?;
+            }
+        }
+
+        tx.commit()?;
+
+        Ok(())
+    }
+
+    /// Get all browser sessions,
+    fn sessions(&self) -> rusqlite::Result<Vec<SessionEntry>> {
+        let mut statement =
+            self.connection.prepare("SELECT pid, window_id, data, uri FROM session")?;
+        let sessions = statement
+            .query_map([], |row| {
+                let pid: u32 = row.get(0)?;
+                let window_id: usize = row.get(1)?;
+                let session_data: Vec<u8> = row.get(2)?;
+                let uri: String = row.get(3)?;
+                Ok(SessionEntry { pid, window_id, session_data, uri })
+            })?
+            .flatten()
+            .collect();
+        Ok(sessions)
+    }
+
+    /// Delete browser sessions.
+    fn delete_sessions(&self, pids: impl IntoIterator<Item = u32>) -> rusqlite::Result<()> {
+        let mut stmt = self.connection.prepare("DELETE FROM session WHERE pid = ?1")?;
+        for pid in pids {
+            stmt.execute([pid])?;
+        }
         Ok(())
     }
 }
@@ -345,6 +477,27 @@ impl HistoryUri {
 impl From<&str> for HistoryUri {
     fn from(s: &str) -> Self {
         Self::new(s)
+    }
+}
+
+/// Browser history session entry.
+#[derive(Debug)]
+pub struct SessionEntry {
+    pub pid: u32,
+    pub window_id: usize,
+    pub session_data: Vec<u8>,
+    pub uri: String,
+}
+
+/// Object for writing sessions to the DB.
+pub struct SessionRecord {
+    pub data: Vec<u8>,
+    pub uri: String,
+}
+
+impl From<&Box<dyn Engine>> for SessionRecord {
+    fn from(engine: &Box<dyn Engine>) -> Self {
+        Self { data: engine.session(), uri: engine.uri() }
     }
 }
 

src/ui/engine_backdrop.rs
@@ -17,10 +17,8 @@ use crate::wayland::protocols::ProtocolStates;
 use crate::{gl, Size, State};
 
 // Fallback color when waiting for engine frame.
-// const SPB_COLOR: u32 = u32::MAX / 10;
-// const GL_COLOR: [f64; 3] = [0.1, 0.1, 0.1];
-const SPB_COLOR: u32 = u32::MAX / 2;
-const GL_COLOR: [f64; 3] = [1.0, 0., 1.0];
+const SPB_COLOR: u32 = u32::MAX / 10;
+const GL_COLOR: [f64; 3] = [0.1, 0.1, 0.1];
 
 /// Single-color engine backdrop surface.
 pub struct EngineBackdrop {

src/ui/overlay/tabs.rs
@@ -85,7 +85,7 @@ impl TabsHandler for State {
             None => return,
         };
 
-        window.close_tab(engine_id);
+        window.close_tab(&self.history, engine_id);
     }
 }
 

src/window.rs
@@ -29,14 +29,14 @@ use smithay_client_toolkit::shell::WaylandSurface;
 
 use crate::engine::webkit::{WebKitEngine, WebKitError};
 use crate::engine::{Engine, EngineId};
-use crate::history::{HistoryMatch, MAX_MATCHES};
+use crate::history::{History, HistoryMatch, SessionRecord, MAX_MATCHES};
 use crate::ui::engine_backdrop::EngineBackdrop;
 use crate::ui::overlay::option_menu::{Borders, OptionMenuId, OptionMenuItem, ScrollTarget};
 use crate::ui::overlay::Overlay;
 use crate::ui::Ui;
 use crate::uri::{SCHEMES, TLDS};
 use crate::wayland::protocols::ProtocolStates;
-use crate::{History, Position, Size, State};
+use crate::{Position, Size, State};
 
 /// Search engine base URI.
 const SEARCH_URI: &str = "https://duckduckgo.com/?q=";
@@ -277,7 +277,7 @@ impl Window {
     }
 
     /// Close a tabs.
-    pub fn close_tab(&mut self, engine_id: EngineId) {
+    pub fn close_tab(&mut self, history: &History, engine_id: EngineId) {
         // Remove engine and get the position it was in.
         let index = match self.tabs.shift_remove_full(&engine_id) {
             Some((index, ..)) => index,
@@ -298,6 +298,9 @@ impl Window {
 
         // Update tabs popup.
         self.overlay.tabs_mut().set_tabs(self.tabs.values(), self.active_tab);
+
+        // Update browser sessions.
+        self.persist_session(history);
 
         // Force tabs UI redraw.
         self.dirty = true;
@@ -857,6 +860,17 @@ impl Window {
 
         // Increment URI visit count for history.
         history.visit(uri);
+
+        // Update browser session.
+        self.persist_session(history);
+    }
+
+    /// Update the window's browser session.
+    ///
+    /// This is used to recover the browser session when restarting Kumo.
+    pub fn persist_session(&self, history: &History) {
+        let session: Vec<_> = self.tabs.values().map(SessionRecord::from).collect();
+        history.set_session(self.id, session);
     }
 
     /// Update an engine's title.
@@ -1056,6 +1070,11 @@ impl WindowId {
     pub fn new() -> Self {
         static NEXT_WINDOW_ID: AtomicUsize = AtomicUsize::new(0);
         Self(NEXT_WINDOW_ID.fetch_add(1, Ordering::Relaxed))
+    }
+
+    /// Get the raw window ID value.
+    pub fn as_raw(&self) -> usize {
+        self.0
     }
 }
 

commit 8ace59ad72995f4fe8c24fae281f7c43b7ac23df

Fix tab creation at startup
This fixes a regression introduced in 288e8da which caused URIs passed
through CLI and recovered sessions to all use the same tab, rather than
creating a new tab for each URI.

src/main.rs
@@ -84,12 +84,14 @@ fn main() -> Result<(), Error> {
     let window_id = state.create_window()?;
 
     // Create an empty tab for loading a new page.
-    let get_empty_tab = |window: &mut Window| -> Result<_, Error> {
-        if window.tabs().len() > 1 {
-            Ok(window.add_tab(false)?)
-        } else {
+    let mut is_first_tab = true;
+    let get_empty_tab = |window: &mut Window, is_first_tab: &mut bool| -> Result<_, Error> {
+        if *is_first_tab {
             window.set_keyboard_focus(KeyboardFocus::None);
+            *is_first_tab = false;
             Ok(window.active_tab())
+        } else {
+            Ok(window.add_tab(false)?)
         }
     };
 
@@ -111,6 +113,7 @@ fn main() -> Result<(), Error> {
             if session_pid.is_some() || session_window_id.is_some() {
                 let new_window_id = state.create_window()?;
                 window = state.windows.get_mut(&new_window_id).unwrap();
+                is_first_tab = true;
             }
 
             session_window_id = Some(entry.window_id);
@@ -118,7 +121,7 @@ fn main() -> Result<(), Error> {
         }
 
         // Restore the session in a new empty tab.
-        let engine_id = get_empty_tab(window)?;
+        let engine_id = get_empty_tab(window, &mut is_first_tab)?;
         if let Some(engine) = window.tabs().get(&engine_id) {
             engine.restore_session(mem::take(&mut entry.session_data));
             engine.load_uri(&entry.uri);
@@ -138,7 +141,7 @@ fn main() -> Result<(), Error> {
     // Spawn a new tab for every CLI argument.
     let window = state.windows.get_mut(&window_id).unwrap();
     for arg in env::args().skip(1) {
-        get_empty_tab(window)?;
+        get_empty_tab(window, &mut is_first_tab)?;
         window.load_uri(arg);
     }
 
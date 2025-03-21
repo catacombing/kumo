catacombing/kumo

commit d40d40fe1ddabcd9427212bceefb44139de41b9b

Fix overly strict URI autocomplete
This fixes two issues with the URI tab completion where it would not
autocomplete in scenarios that are slightly ambiguous.

Since path segments are case-sensitive, the input URI previously
required fully matching case for comparison. Instead Kumo now uses the
more sensible smart case to allow matching any case when lowercase is
used.

The `www` subdomain is commonly supported by many websites as an
optional prefix, some even redirecting to the `www` subdomain as the
default. To accomodate for this, autocompletion now ignores the `www`
subdomain and suggests path segments across both, since websites are
unlikely to support this subdomain without sharing paths with the
non-subdomain version.

Subdomains other than `www` still have separated autocompletion, since
they're generally less likely to share path segments with each other.

Closes #76.

src/storage/history.rs
@@ -385,9 +385,16 @@ impl HistoryUri {
             return false;
         }
 
+        // Ignore `www` subdomain since it is frequently optional.
+        let base = if !input_uri.base.starts_with("www.") {
+            self.base.strip_prefix("www.").unwrap_or(&self.base)
+        } else {
+            &self.base
+        };
+
         // Check if input is submatch of base without any path segments.
-        if self.base != input_uri.base {
-            return input_uri.path.is_empty() && self.base.starts_with(&input_uri.base);
+        if base != input_uri.base {
+            return input_uri.path.is_empty() && base.starts_with(&input_uri.base);
         }
 
         // Abort if input is longer than URI.
@@ -397,9 +404,17 @@ impl HistoryUri {
         }
 
         // Check for difference in path segments.
+        let ignore_case = input_uri.is_lowercase();
         for (i, (segment, input_segment)) in self.path.iter().zip(&input_uri.path).enumerate() {
+            // Allow case mismatch with fully lowercased input URI.
+            let segment = if ignore_case {
+                Cow::Owned(segment.to_lowercase())
+            } else {
+                Cow::Borrowed(segment)
+            };
+
             if !segment.starts_with(input_segment)
-                || (input_segment != segment && i + 1 != input_path_len)
+                || (*input_segment != *segment && i + 1 != input_path_len)
             {
                 return false;
             }
@@ -414,6 +429,16 @@ impl HistoryUri {
     /// avoid bloating the history with multiple URIs per target resource.
     fn normalize(&mut self) {
         self.path.retain(|p| !p.is_empty())
+    }
+
+    /// Check if all characters are lowercase.
+    ///
+    /// Ambiguous characters that are neither upper- nor lowercase are
+    /// considered lowercase.
+    fn is_lowercase(&self) -> bool {
+        self.scheme.chars().all(|c| !c.is_uppercase())
+            && self.base.chars().all(|c| !c.is_uppercase())
+            && self.path.iter().flat_map(|p| p.chars()).all(|c| !c.is_uppercase())
     }
 }
 
@@ -570,5 +595,25 @@ mod tests {
         assert!(!uri.autocomplete(&"example.org/path/segmen/".into()));
         assert!(!uri.autocomplete(&"example.org/path//segment".into()));
         assert!(!uri.autocomplete(&"example.org//".into()));
+    }
+
+    #[test]
+    fn www_subdomain_autocomplete() {
+        let uri = HistoryUri::new("https://www.example.org/one/two/three");
+        assert!(uri.autocomplete(&"https://www.example.org/".into()));
+        assert!(uri.autocomplete(&"https://example.org/".into()));
+        assert!(uri.autocomplete(&"example.org/on".into()));
+        assert!(uri.autocomplete(&"example.org/".into()));
+        assert!(uri.autocomplete(&"example.org".into()));
+    }
+
+    #[test]
+    fn ignore_case_autocomplete() {
+        let uri = HistoryUri::new("https://example.org/One/Two/Three");
+        assert!(uri.autocomplete(&"example.org/one/two/thre".into()));
+        assert!(uri.autocomplete(&"example.org/one/two".into()));
+        assert!(uri.autocomplete(&"example.org/one/".into()));
+
+        assert!(!uri.autocomplete(&"example.org/One/tw".into()));
     }
 }

commit dc39ada03cf3aefb2c0e5d6443c61b46927494c3

Fix autocompletions with `www` subdomains
This fixes a regression introduced in d40d40f which caused Kumo to
suggests `www.` prefixed URLs as autocompletions, without stripping the
subdomain prefix, causing odd behavior.

This also fixes another related regression where an `www` prefix in the
input uri wouldn't be autocompleted until the subdomain was completely
entered.

src/storage/history.rs
@@ -149,6 +149,16 @@ impl History {
             .iter()
             .filter(|(uri, _)| uri.autocomplete(&input_uri))
             .max_by_key(|(_, entry)| entry.views)?;
+        let mut uri = Cow::Borrowed(uri);
+
+        // Strip `www` from suggestion if input did not contain it.
+        if !input_uri.base.starts_with("www.") && !"www".starts_with(&input_uri.base) {
+            let mut new_uri = HistoryUri::clone(&uri);
+            if let Some(base) = uri.base.strip_prefix("www.") {
+                new_uri.base = base.into();
+            }
+            uri = Cow::Owned(new_uri);
+        }
 
         let mut uri_str = uri.to_string(!input_uri.scheme.is_empty());
 
@@ -386,7 +396,7 @@ impl HistoryUri {
         }
 
         // Ignore `www` subdomain since it is frequently optional.
-        let base = if !input_uri.base.starts_with("www.") {
+        let base = if !input_uri.base.starts_with("www.") && !"www".starts_with(&input_uri.base) {
             self.base.strip_prefix("www.").unwrap_or(&self.base)
         } else {
             &self.base
@@ -595,6 +605,17 @@ mod tests {
         assert!(!uri.autocomplete(&"example.org/path/segmen/".into()));
         assert!(!uri.autocomplete(&"example.org/path//segment".into()));
         assert!(!uri.autocomplete(&"example.org//".into()));
+    }
+
+    #[test]
+    fn subdomain_autocomplete() {
+        let uri = HistoryUri::new("https://www.example.org/one/two/three");
+        assert!(!uri.autocomplete(&"ww.".into()));
+        assert!(uri.autocomplete(&"ww".into()));
+
+        let uri = HistoryUri::new("https://catacomb.example.org/one/two/three");
+        assert!(!uri.autocomplete(&"ca.".into()));
+        assert!(uri.autocomplete(&"ca".into()));
     }
 
     #[test]